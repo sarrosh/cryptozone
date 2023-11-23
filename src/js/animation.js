b8e348538be1461c8a95c17e7a47f06982430542
sarrosh <151837652+sarrosh@users.noreply.github.com>
2023-11-23 03:53:14 -0800

    First Upload

---
+++ src/js/animation.js
@@ -0,0 +1,19 @@
+function defaultAnimations() {
+	document.querySelector('.nav-btn-1').style.animation = 'navappear 0.6s ease-in-out forwards';
+	document.querySelector('.nav-btn-2').style.animation = 'navappear 0.6s ease-in-out 0.1s forwards';
+	document.querySelector('.nav-btn-3').style.animation = 'navappear 0.6s ease-in-out 0.2s forwards';
+	document.querySelector('.nav-btn-4').style.animation = 'navappear 0.6s ease-in-out 0.3s forwards';
+	document.querySelector('.nav-btn-5').style.animation = 'navappear 0.6s ease-in-out 0.4s forwards';
+	document.querySelector('.logo-bar').style.animation = 'logoappear 0.6s ease-in-out 0.2s forwards';
+	document.querySelector('.glow-line-1').style.animation = 'glappear 1.2s ease-in-out forwards';
+	document.querySelector('.after-gl-text-1').style.animation = 'leftappear 0.6s ease-in-out 0.2s forwards';
+	document.querySelector('.after-gl-text-2').style.animation = 'leftappear 0.6s ease-in-out 0.3s forwards';
+	document.querySelector('.sec-1-left .down').style.animation = 'leftappear2 0.6s ease-in-out 0.4s forwards';
+	document.querySelector('.sec-1-eye-catcher').style.animation = 'eyecappear 0.6s ease-in-out 0.4s forwards';
+	document.querySelector('.sec-1-eye-catcher2').style.animation = 'eyecappear2 0.6s ease-in-out 0.4s forwards';
+	setTimeout(()=>{
+		document.querySelector('.sec-1-eye-catcher2').style.animation = 'infinityupdown 4s ease-in-out infinite';
+	},1700);
+	document.querySelector('.bitcoin-logo').style.animation = 'eyecappear3 0.6s ease-in-out 0.8s forwards';
+	document.querySelector('.pointer-arrow').style.animation = 'justappear 0.6s ease-in-out 0.3s forwards';
+}
---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function moveBitCoin() {
+	document.querySelector('.bitcoin-logo').style.scale = '1.06';
+	document.querySelector('.bitcoin-logo').style.rotate = '20deg';
+}
+function removeBitCoin() {
+	document.querySelector('.bitcoin-logo').style.scale = '1.0';
+	document.querySelector('.bitcoin-logo').style.rotate = '0deg';
+}
